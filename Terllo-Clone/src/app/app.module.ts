724d6b73630cafe28d885378c5c2962ebe6ddb6d
Mariam Mahareeq <52622730+mmahareeq@users.noreply.github.com>
2023-09-30 12:15:56 +0300

    fetch board data and set state managment

--- Terllo-Clone/src/app/app.module.ts
+++ Terllo-Clone/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { reducers } from './ngrx-store/board-column.reducer'
+import { AppState } from './ngrx-store/board-column.state';
+import { ActionReducerMap, StoreModule } from '@ngrx/store';
+import { ManagerBoardModule } from '../manager-board/manager-board.module.ts';
 
+export const reducer: ActionReducerMap<AppState, any> = {
+  board: reducers
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    StoreModule.forRoot(reducer),
+    BrowserModule,
+    ManagerBoardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Terllo-Clone/src/app/manager-board/board/board.component.ts
+++ Terllo-Clone/src/app/manager-board/board/board.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable} from 'rxjs';
+
+import {AppState } from 'src/app/ngrx-store/board-column.state.ts';
+import { Column } from '../models/column.model';
 
 @Component({
   selector: 'app-board',
@@ -6,10 +11,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-
-  constructor() { }
+  
+  board!: Observable<{columns: column[]}>
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.board = this.store.select('board');
   }
 
 }
